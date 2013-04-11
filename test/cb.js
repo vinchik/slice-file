0a0dba9c68c408326a7f6cc5d64798dc81b38c14
James Halliday <mail@substack.net>
2013-04-10 20:35:22 -0700

    failing cb test for implicit start=0

--- test/cb.js
+++ test/cb.js
@@ -10,3 +10,17 @@ test('slice callback', function (t) {
         t.deepEqual(lines.map(String), [ 'five\n', 'six\n' ]);
     });
 });
+
+test('implicit slice 0 callback', function (t) {
+    t.plan(1);
+    
+    var file = __dirname + '/data/lines.txt';
+    lf(file).slice(function (err, lines) {
+        if (err) t.fail(err);
+        var res = lines.map(String);
+        t.deepEqual(res, [
+            "\n", "two\n", "three\n", "\n",
+            "five\n", "six\n", "\n"
+        ]);
+    });
+});
