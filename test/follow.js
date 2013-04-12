fdd0bf16fa05912c349033e673c582a1664d1a2a
James Halliday <mail@substack.net>
2013-04-12 03:12:36 -0700

    check the newline in the test itself

--- test/follow.js
+++ test/follow.js
@@ -13,31 +13,31 @@ test(function (t) {
     var xs = sf(file);
     var res = [];
     xs.follow(-3).pipe(through(function (line) {
-        res.push(String(line).trim());
+        res.push(String(line));
         if (res.length === 3) {
-            t.deepEqual(res, [ 'four', 'five', 'six' ]);
+            t.deepEqual(res, [ 'four\n', 'five\n', 'six\n' ]);
             fs.appendFile(file, [ 'seven', 'eight', 'nine', '' ].join('\n'));
         }
         else if (res.length === 6) {
             t.deepEqual(res, [
-                'four', 'five', 'six',
-                'seven', 'eight', 'nine'
+                'four\n', 'five\n', 'six\n',
+                'seven\n', 'eight\n', 'nine\n'
             ]);
             fs.appendFile(file, [ 'ten', 'eleven' ].join('\n'));
         }
         else if (res.length === 7) {
             t.deepEqual(res, [
-                'four', 'five', 'six',
-                'seven', 'eight', 'nine',
-                'ten'
+                'four\n', 'five\n', 'six\n',
+                'seven\n', 'eight\n', 'nine\n',
+                'ten\n'
             ]);
             fs.appendFile(file, '\n');
         }
         else if (res.length === 8) {
             t.deepEqual(res, [
-                'four', 'five', 'six',
-                'seven', 'eight', 'nine',
-                'ten', 'eleven'
+                'four\n', 'five\n', 'six\n',
+                'seven\n', 'eight\n', 'nine\n',
+                'ten\n', 'eleven\n'
             ]);
             xs.close();
         }
